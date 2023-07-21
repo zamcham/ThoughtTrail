2ce15f7cfe76b176310011affc24182282cf1368
Cristian Zambrano <criszamcham@gmail.com>
2023-07-21 17:36:36 -0400

    fix linters

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe Like, type: :model do
 
   describe '#increment_like_count' do
     it 'increments the likes_counter of the associated post' do
-      like = Like.create(user:, post:)
+      like = Like.create(user: user, post: post)
       expect { like.increment_like_count }.to change { post.reload.likes_counter }.by(1)
     end
   end
 
   describe '#decrement_like_count' do
     it 'decrements the likes_counter of the associated post' do
-      like = Like.create(user:, post:)
+      like = Like.create(user: user, post: post)
       expect { like.decrement_like_count }.to change { post.reload.likes_counter }.by(-1)
     end
   end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,42 +5,42 @@ RSpec.describe Post, type: :model do
   let(:user) { User.create(name: 'John Doe') }
 
   it 'is valid with a title, comments_counter, and likes_counter' do
-    post = Post.new(title: 'Sample Title', comments_counter: 5, likes_counter: 10, user:)
+    post = Post.new(title: 'Sample Title', comments_counter: 5, likes_counter: 10, user: user)
     expect(post).to be_valid
   end
 
   it 'is invalid without a title' do
-    post = Post.new(title: nil, comments_counter: 5, likes_counter: 10, user:)
+    post = Post.new(title: nil, comments_counter: 5, likes_counter: 10, user: user)
     expect(post).not_to be_valid
     expect(post.errors[:title]).to include("can't be blank")
   end
 
   it 'is invalid with a title exceeding 250 characters' do
-    post = Post.new(title: 'A' * 251, comments_counter: 5, likes_counter: 10, user:)
+    post = Post.new(title: 'A' * 251, comments_counter: 5, likes_counter: 10, user: user)
     expect(post).not_to be_valid
     expect(post.errors[:title]).to include('is too long (maximum is 250 characters)')
   end
 
   it 'is invalid with a negative comments_counter' do
-    post = Post.new(title: 'Sample Title', comments_counter: -1, likes_counter: 10, user:)
+    post = Post.new(title: 'Sample Title', comments_counter: -1, likes_counter: 10, user: user)
     expect(post).not_to be_valid
     expect(post.errors[:comments_counter]).to include('must be greater than or equal to 0')
   end
 
   it 'is invalid with a negative likes_counter' do
-    post = Post.new(title: 'Sample Title', comments_counter: 5, likes_counter: -1, user:)
+    post = Post.new(title: 'Sample Title', comments_counter: 5, likes_counter: -1, user: user)
     expect(post).not_to be_valid
     expect(post.errors[:likes_counter]).to include('must be greater than or equal to 0')
   end
 
   it "increments the user's posts_counter after creation" do
     expect do
-      Post.create(title: 'Sample Title', comments_counter: 5, likes_counter: 10, user:)
+      Post.create(title: 'Sample Title', comments_counter: 5, likes_counter: 10, user: user)
     end.to change { user.reload.posts_counter }.by(1)
   end
 
   it "decrements the user's posts_counter after destruction" do
-    post = Post.create(title: 'Sample Title', comments_counter: 5, likes_counter: 10, user:)
+    post = Post.create(title: 'Sample Title', comments_counter: 5, likes_counter: 10, user: user)
     expect do
       post.destroy
     end.to change { user.reload.posts_counter }.by(-1)
@@ -49,7 +49,7 @@ RSpec.describe Post, type: :model do
   it 'returns the 5 most recent comments' do
     user1 = User.new(name: 'Burger Doe', id: 1)
     post = Post.create(title: 'Sample Title', comments_counter: 5, likes_counter: 10, author_id: user1.id)
-    older_comment = Comment.create(post:, text: 'Older Comment')
+    older_comment = Comment.create(post: post, text: 'Older Comment')
 
     expect(post.recent_comments).not_to include(older_comment)
   end
